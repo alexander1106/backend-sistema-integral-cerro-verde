f427abaa5771686ff1a287037094d638834e459b
furawa23 <fabrizzioflores2014@gmail.com>
2025-04-19 10:42:54 -0500

    repositories de caja agregados

---
+++ src/main/java/com/alexander/sistema_cerro_verde_backend/repository/caja/CajasRepository.java
@@ -0,0 +1,7 @@
+package com.alexander.sistema_cerro_verde_backend.repository.caja;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alexander.sistema_cerro_verde_backend.entity.caja.Cajas;
+
+public interface CajasRepository extends JpaRepository<Cajas, Integer> {}
